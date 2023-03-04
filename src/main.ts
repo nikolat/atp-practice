54f3b7160ec7ccec5e1fd1cb193b923541369bfa
Don <evidence@384.jp>
2023-03-04 18:14:28 +0900

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,146 @@
+// https://gist.github.com/jiftechnify/1a6cfb1b3c1062b65978713010c5422a
+import { AtpAgent } from "@atproto/api";
+
+(() => {
+	const getTLButton = <HTMLButtonElement>document.getElementById("gettl");
+	const getProfileButton = <HTMLButtonElement>document.getElementById("getprofile");
+	const setProfileButton = <HTMLButtonElement>document.getElementById("setprofile");
+	getTLButton.addEventListener("click", () => {
+		(<HTMLElement>document.getElementById("log")).textContent = "";
+		(<HTMLElement>document.getElementById("error")).textContent = "";
+		const emailInput = <HTMLInputElement>document.getElementById("email");
+		const passwordInput = <HTMLInputElement>document.getElementById("password");
+		if (!emailInput.value || !passwordInput.value){
+			return;
+		}
+		const main = async () => {
+			const agent = new AtpAgent({
+				service: "https://bsky.social",
+			});
+	
+			await agent.login({
+				identifier: emailInput.value,
+				password: passwordInput.value,
+			});
+	
+			const dl = <HTMLElement>document.getElementById("tl");
+			const tl = await agent.api.app.bsky.feed.getTimeline({});
+			for (const p of tl.data.feed) {
+				console.log(p);
+				const dt = <HTMLElement>document.createElement("dt");
+				dt.textContent = p.post.author.displayName + ' ' + p.post.author.handle;
+				const dd = <HTMLElement>document.createElement("dd");
+				dd.textContent = (p.post.record as any).text + " ↻" + p.post.repostCount + " ♥" + p.post.upvoteCount;
+				dl.appendChild(dt);
+				dl.appendChild(dd);
+			}
+		};
+		main()
+			.then(() => {
+				console.log("fin");
+				(<HTMLElement>document.getElementById("log")).textContent = "complete";
+				(<HTMLElement>document.getElementById("error")).textContent = "";
+			})
+			.catch((e) => {
+				console.error(e);
+				(<HTMLElement>document.getElementById("log")).textContent = "";
+				(<HTMLElement>document.getElementById("error")).textContent = e.message;
+			});
+	});
+	getProfileButton.addEventListener("click", () => {
+		(<HTMLElement>document.getElementById("log")).textContent = "";
+		(<HTMLElement>document.getElementById("error")).textContent = "";
+		const emailInput = <HTMLInputElement>document.getElementById("email");
+		const passwordInput = <HTMLInputElement>document.getElementById("password");
+		const account = <HTMLInputElement>document.getElementById("account");
+		if (!emailInput.value || !passwordInput.value || !account.value){
+			return;
+		}
+		const main = async () => {
+			const agent = new AtpAgent({
+				service: "https://bsky.social",
+			});
+			const actor = account.value + '.bsky.social';
+			await agent.login({
+				identifier: emailInput.value,
+				password: passwordInput.value,
+			});
+			const prof = await agent.api.app.bsky.actor.getProfile({actor: actor});
+			const avatardt = <HTMLElement>document.getElementById("avatar-dt");
+			avatardt.innerHTML = "";
+			const img = <HTMLImageElement>document.createElement("img");
+			img.setAttribute("src", prof.data.avatar || "");
+			img.setAttribute("width", "50");
+			img.setAttribute("height", "50");
+			avatardt.appendChild(img);
+			const avatar = <HTMLInputElement>document.getElementById("avatar");
+			avatar.value = prof.data.avatar || "";
+			const displayName = <HTMLInputElement>document.getElementById("displayname");
+			displayName.value = prof.data.displayName || "";
+			const description = <HTMLInputElement>document.getElementById("description");
+			description.value = prof.data.description || "";
+			console.log(prof);
+		};
+		main()
+			.then(() => {
+				console.log("fin");
+				(<HTMLElement>document.getElementById("log")).textContent = "complete";
+				(<HTMLElement>document.getElementById("error")).textContent = "";
+			})
+			.catch((e) => {
+				console.error(e);
+				(<HTMLElement>document.getElementById("log")).textContent = "";
+				(<HTMLElement>document.getElementById("error")).textContent = e.message;
+			});
+	});
+	setProfileButton.addEventListener("click", () => {
+		(<HTMLElement>document.getElementById("log")).textContent = "";
+		(<HTMLElement>document.getElementById("error")).textContent = "";
+		const emailInput = <HTMLInputElement>document.getElementById("email");
+		const passwordInput = <HTMLInputElement>document.getElementById("password");
+		const account = <HTMLInputElement>document.getElementById("account");
+		if (!emailInput.value || !passwordInput.value || !account.value){
+			return;
+		}
+		const main = async () => {
+			const agent = new AtpAgent({
+				service: "https://bsky.social",
+			});
+			const actor = account.value + '.bsky.social';
+			await agent.login({
+				identifier: emailInput.value,
+				password: passwordInput.value,
+			});
+			const avatar = <HTMLInputElement>document.getElementById("avatar");
+			const displayName = <HTMLInputElement>document.getElementById("displayname");
+			const description = <HTMLInputElement>document.getElementById("description");
+
+			const response = await fetch(avatar.value);
+			const iconImageBlob: Blob = await response.blob();
+			const arrayBuffer: ArrayBuffer = await iconImageBlob.arrayBuffer();
+			const iconImage: Uint8Array = new Uint8Array(arrayBuffer);
+
+			const uploadIconResp = await agent.api.com.atproto.blob.upload(iconImage, {
+				encoding: "image/png", // PNGの場合は "image/jpeg" にする
+			});
+			const createProfileResp = await agent.api.app.bsky.actor.updateProfile({
+				displayName: displayName.value,
+				description: description.value,
+				avatar: { cid: uploadIconResp.data.cid, mimeType: "image/png" },  // PNGの場合は "image/jpeg" にする
+				banner: null
+			});
+			console.log(createProfileResp);
+		};
+		main()
+			.then(() => {
+				console.log("fin");
+				(<HTMLElement>document.getElementById("log")).textContent = "complete";
+				(<HTMLElement>document.getElementById("error")).textContent = "";
+			})
+			.catch((e) => {
+				console.error(e);
+				(<HTMLElement>document.getElementById("log")).textContent = "";
+				(<HTMLElement>document.getElementById("error")).textContent = e.message;
+			});
+	});
+})();
